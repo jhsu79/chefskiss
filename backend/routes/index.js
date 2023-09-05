e5ca8e86d829a171d4a40bb9a82f9e622bbdf4b3
jaredden <james.ifit@gmail.com>
2023-09-05 16:06:05 -0400

    update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-/* localhost 4000 */
 router.get('/', function(req, res, next) {
   res.send('test');
 });
-
 module.exports = router;
