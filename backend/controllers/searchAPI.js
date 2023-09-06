8e9c43d423227f5b20f9cfc07f9d72a1c0e153d1
jaredden <james.ifit@gmail.com>
2023-09-06 15:47:51 -0400

    cleanup dead code and add notes

--- backend/controllers/searchAPI.js
+++ backend/controllers/searchAPI.js
@@ -3,11 +3,6 @@ const YELP_API_KEY = process.env.YELP_API_KEY;
 
 module.exports = {
     searchYelp,
-    // searchAPI,
-    // indexAPI,
-    // getCurrencies,  
-    // getLanguages,
-    // getTypeahead,
   };
 
   async function searchYelp(req, res) {
@@ -20,8 +15,8 @@ module.exports = {
             location,
             term: 'restaurants',
             sort_by: 'distance',
-            radius: 40000, //20 miles since Yelp requires radius to be in meters
-            limit: 5, 
+            radius: 40000, // 20 miles since Yelp requires radius to be in meters
+            limit: 5, // Limiting the search result
         },
         headers: {
             'Authorization': `Bearer ${YELP_API_KEY}`
@@ -35,117 +30,3 @@ module.exports = {
         res.status(400).json({ error: error.message });
     }
 }
-
-
-
-
-//   async function searchAPI(req, res) {
-//   const { location_id = '', language = 'en_US', currency = 'USD', offset = '0' } = req.query;
-
-//   const encodedParams = new URLSearchParams();
-//   encodedParams.set('language', language);
-//   encodedParams.set('location_id', location_id);
-//   encodedParams.set('currency', currency);
-//   encodedParams.set('offset', offset);
-
-//   const options = {
-//       method: 'POST',
-//       url: 'https://worldwide-restaurants.p.rapidapi.com/search',
-//       headers: {
-//           'content-type': 'application/x-www-form-urlencoded',
-//           'X-RapidAPI-Key': 'process.env.RAPIDAPI_KEY',  
-//           'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-//       },
-//       data: encodedParams,
-//   };
-
-//   try {
-//       const response = await axios.request(options);
-//       console.log("here are the options", options)
-//       console.log("this is the response", response)
-//       res.status(200).json(response.data);
-//   } catch (error) {
-//       res.status(400).json({ error: error.message });
-//   }
-// }
-
-// async function indexAPI(req, res) {
-//   const { location_id, typeahead = 'band', language = 'en_US', currency = 'USD' } = req.query;
-
-//   const encodedParams = new URLSearchParams();
-//   encodedParams.set('currency', currency);
-//   encodedParams.set('language', language);
-//   encodedParams.set('location_id', location_id);
-//   encodedParams.set('q', typeahead);
-
-//   const options = {
-//       method: 'POST',
-//       url: 'https://worldwide-restaurants.p.rapidapi.com/detail',
-//       headers: {
-//           'content-type': 'application/x-www-form-urlencoded',
-//           'X-RapidAPI-Key': 'process.env.RAPIDAPI_KEY', 
-//           'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-//       },
-//       data: encodedParams,
-//   };
-
-//   try {
-//       const response = await axios.request(options);
-//       res.status(200).json(response.data);
-//   } catch (error) {
-//       res.status(400).json({ error: error.message });
-//   }
-// }
-// async function getCurrencies(req, res) {
-//   const options = {
-//       method: 'GET',
-//       url: 'https://worldwide-restaurants.p.rapidapi.com/currencies',
-//       headers: {
-//           'X-RapidAPI-Key': RAPIDAPI_KEY,  // Use the variable
-//           'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-//       }
-//   };
-
-//   try {
-//       const response = await axios.request(options);
-//       res.status(200).json(response.data);
-//   } catch (error) {
-//       res.status(400).json({ error: error.message });
-//   }
-// }
-
-// async function getLanguages(req, res) {
-//   const options = {
-//       method: 'GET',
-//       url: 'https://worldwide-restaurants.p.rapidapi.com/languages',
-//       headers: {
-//           'X-RapidAPI-Key': RAPIDAPI_KEY,  // Use the variable
-//           'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-//       }
-//   };
-
-//   try {
-//       const response = await axios.request(options);
-//       res.status(200).json(response.data);
-//   } catch (error) {
-//       res.status(400).json({ error: error.message });
-//   }
-// }
-
-// async function getTypeahead(req, res) {
-//   const options = {
-//       method: 'POST',
-//       url: 'https://worldwide-restaurants.p.rapidapi.com/typeahead',
-//       headers: {
-//           'X-RapidAPI-Key': RAPIDAPI_KEY,  // Use the variable
-//           'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-//       }
-//   };
-
-//   try {
-//       const response = await axios.request(options);
-//       res.status(200).json(response.data);
-//   } catch (error) {
-//       res.status(400).json({ error: error.message });
-//   }
-// }
