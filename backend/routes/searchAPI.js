51e04637e3b94ea8be2fbebd098550ec7aeda585
jaredden <james.ifit@gmail.com>
2023-09-06 10:42:01 -0400

    update route

--- backend/routes/searchAPI.js
+++ backend/routes/searchAPI.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const searchAPICtrl = require('../controllers/searchAPI');
 
 // External service routes
-router.get('/search', searchCtrl.index); // shows search results
 router.post('/search/search-external', searchCtrl.searchAPI);
-router.post('/search/detail-external', searchCtrl.showAPI);
+router.post('/search/detail-external', searchCtrl.indexAPI);
 router.get('/search/currencies', searchCtrl.getCurrencies);
 router.get('/search/languages', searchCtrl.getLanguages);
 router.get('/search/typeahead', searchCtrl.getTypeahead);
