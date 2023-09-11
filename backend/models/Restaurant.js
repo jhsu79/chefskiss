59026081a2665d49df44cb16a6105f9b357d5c61
jhsu79 <joshuanhsu@gmail.com>
2023-09-11 11:12:11 -0400

    stashing changes

--- backend/models/Restaurant.js
+++ backend/models/Restaurant.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
 const restaurantSchema = new Schema(
   {
     name: { type: String },
-<<<<<<< HEAD
-=======
-    // categories: [{ type: String }],
->>>>>>> 85d1c9a3dd2912265efe99cff1ef84d3c0e745d7
     display_phone: { type: String },
     display_address0: { type: String },
     display_address1: { type: String },
