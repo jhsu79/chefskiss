5c02e0baff75f0d47ba4bed7d87f42e3d52b1dcf
jhsu79 <joshuanhsu@gmail.com>
2023-09-11 11:42:13 -0400

    tested and hit routes for impressions

--- backend/controllers/impressions.js
+++ backend/controllers/impressions.js
@@ -3,12 +3,15 @@ const { Restaurant } = require("../models");
 module.exports = {
     create, 
 }
-async function create (req, res) {
+
+async function create(req, res) {
     const restaurant = await Restaurant.findById(req.params.id)
     restaurant.impression.push(req.body) 
-try {
-    await restaurant.save()
-} catch (err) {
-    console.log(err)
-}
-}
+    try {
+      console.log(req.body);
+      res.status(201).json(await restaurant.save());
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: error.message });
+    }
+  }
