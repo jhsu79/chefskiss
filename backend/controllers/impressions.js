cad3bd3fecb62562f915b3cc506749d216244193
jaredden <james.ifit@gmail.com>
2023-09-11 17:59:35 -0400

    cleaned up controllers/impressions

--- backend/controllers/impressions.js
+++ backend/controllers/impressions.js
@@ -1,18 +1,17 @@
 const { Restaurant } = require("../models");
 
 module.exports = {
-    create, 
-}
+  create,
+};
 
 async function create(req, res) {
-    const restaurant = await Restaurant.findById(req.params.id)
-    console.log("i'm in the create function")
-    restaurant.impression = req.body 
-    try {
-      console.log(req.body);
-      res.status(201).json(await restaurant.save());
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ error: error.message });
-    }
+  const restaurant = await Restaurant.findById(req.params.id);
+  restaurant.impression = req.body;
+  try {
+    console.log(req.body);
+    res.status(201).json(await restaurant.save());
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
   }
+}
